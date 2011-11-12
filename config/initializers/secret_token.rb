9dfddfcfbf501ff18a8a41342a988e9ad5c1776d
Sriprasanth Balaras <prish@windowslive.com>
2011-11-13 09:51:56 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prish::Application.config.secret_token = '87fed78724e3c8dfc747e3996599722ed6c468e70e7bc7ac6278b81488af447a0bbc1aca56cfe25221a4afd8bcf50e8e0f7261ee18953e4a86084fe08e35eb0e'
